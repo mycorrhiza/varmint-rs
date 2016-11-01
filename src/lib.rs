848737a6db87cf450cda744bb5c3a0d49748c926
Wim Looman <wim@nemo157.com>
2016-11-01 19:27:57 +0100

    Add more builtin rustc warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+#![warn(box_pointers)]
+#![warn(fat_ptr_transmutes)]
+#![warn(missing_copy_implementations)]
+#![warn(missing_debug_implementations)]
+#![warn(missing_docs)]
+#![warn(trivial_casts)]
+#![warn(trivial_numeric_casts)]
+#![warn(unused_extern_crates)]
+#![warn(unused_import_braces)]
+#![warn(unused_results)]
+#![warn(variant_size_differences)]
+
+
 mod read;
 mod write;
 
